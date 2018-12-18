2f5bbabecd883c7d39f349711d359a09438ed027
jonas2305 <jona535h>
2018-12-18 08:34:04 +0100

    Filnavne ændret og logo tilføjet

--- index.php
+++ index.php
@@ -25,6 +25,47 @@
 </head>
 <body>
 
+<!--HEADER-->
+<header>
+    <!--Logo til Mobil-->
+    <nav class="logo-mobil">
+        <figure>
+            <a href="index.php" class="logo">
+                <img src="images/logo/logo-02.pngpng" alt="Netværk for yngre kræftramte logo">
+            </a>
+        </figure>
+    </nav>
+    <!--Logo til WEB-->
+    <nav class="logo-web">
+        <figure>
+            <a href="index.php" class="logo">
+                <img src="images/logo/logo-02.png" alt="Netværk for yngre kræftramte logo">
+            </a>
+        </figure>
+    </nav>
+    <!--Selve navigationen-->
+    <nav class="topnav" id="myTopnav">
+        <!--Menuknap til burgermenu-->
+        <div class="menuknap">
+            <i class="fas fa-bars"></i>
+        </div>
+        <!--Menu-->
+        <div class="topnavContentWrapper">
+            <!--Lukkeknap til burgermenu-->
+            <i class="fas fa-times burgermenuClose"></i>
+            <!--Menupunkter-->
+            <div class="topnavContent">
+                <a class="menuActive" href="index.php">Forside</a>
+                <a href="arrangementer.php">Arrangementer</a>
+                <a href="hvemervi.php">Hvem er vi?</a>
+                <a href="blog.php">Blog</a>
+                <a href="sponsorer.php">Sponsorer</a>
+                <a href="kontakt.php">Kontakt</a>
+            </div>
+        </div>
+    </nav>
+</header>
+
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 <script src="js/script.js"></script>
---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+$(document).ready (function () {
+
+    //jQuery til BURGERMENU ÅBEN
+    $('.menuknap').click(function () {
+        $('.topnavContentWrapper').show();
+    });
+    //jQuery til BURGERMENU LUKKE
+    $('.burgermenuClose').click(function () {
+        $('.topnavContentWrapper').hide();
+    });
+
+
+});
